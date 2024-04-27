8a4f6d8fd23821ac4202d86b19c5896ad5ddbddb
Sudono Tanjung <sudtanj.business@gmail.com>
2024-04-27 09:39:55 +0700

    Update minerd.sh

--- scripts/minerd.sh
+++ scripts/minerd.sh
@@ -5,4 +5,4 @@ URL="stratum+ssl://scrypt.auto.nicehash.com:443"
 EMAIL="31up6QwFj8JA5dtQyTB1MtGx3yDoQQ2wE4"
 THREADS="4"
 
-minerd -a $ALGO --url=$URL --user=$EMAIL -p x -t $THREADS
+./cpuminer -a $ALGO --url=$URL --user=$EMAIL -p x -t $THREADS
