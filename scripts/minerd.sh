55a6088a5295fb921c5c954c0ce81f1345e3c898
Sudono Tanjung <sudtanj.business@gmail.com>
2024-04-27 09:12:43 +0700

    Update minerd.sh

--- scripts/minerd.sh
+++ scripts/minerd.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+ALGO="Scrypt"
+URL="stratum+ssl://scrypt.auto.nicehash.com:443"
+EMAIL="31up6QwFj8JA5dtQyTB1MtGx3yDoQQ2wE4"
+THREADS="4"
+
 minerd -a $ALGO --url=$URL --user=$EMAIL -p x -t $THREADS
